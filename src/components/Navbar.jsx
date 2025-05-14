0e1d79c83e00a627101de8587a98dd09847fff6e
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-05-14 12:12:44 +0300

    added a navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
+import DarkModeToggle from "./DarkModeToggle";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1> Music Search</h1>
+      <DarkModeToggle />
     </nav>
   );
 };
